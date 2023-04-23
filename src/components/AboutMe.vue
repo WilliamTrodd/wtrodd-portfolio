757fd5caa29bfebabd6f0b5b778a341333730493
WilliamTrodd <william.trodd@gmail.com>
2023-04-23 09:34:57 +0300

    Update AboutMe.vue

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -14,8 +14,8 @@ const active = ref('AboutMe')
       class='cursor-pointer text-center mx-2 pb-2 border-b-2 border-solid w-36 transition-all duration-500'
       :class="active === 'Skills' ? 'border-white' : 'border-primary-blue '">Skills</button>
   </div>
-  <div class='flex flex-wrap items-center justify-around h-[306px]'>
-    <img src='/web-developer.svg' class='w-56 lg:w-80' />
+  <div class='flex flex-col md:flex-row items-center md:justify-center justify-start pb-2 gap-2'>
+    <img src='/web-developer.svg' class='w-56 md:w-2/5' />
     <div class='flex flex-col'>
       <div v-if="active === 'AboutMe'">
         <p class='text-lg w-96'>
